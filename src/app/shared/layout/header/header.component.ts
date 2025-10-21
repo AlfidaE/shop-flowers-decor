0ffd83536e37c5b41b708e3637513e1313838957
«AlfidaE» <ermukhametovaalfida@mail.ru>
2025-10-21 23:21:18 +0500

    First

---
+++ src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,47 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {AuthService} from "../../../core/auth/auth.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  isLogged: boolean = false;
+  @Input() categories: CategoryWithTypeType[] = [];
+
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router,) {
+    this.isLogged = this.authService.getIsLoggedIn()
+  }
+
+  ngOnInit(): void {
+    this.authService.isLogged$.subscribe((isLoggedIn: boolean) => {
+      this.isLogged = isLoggedIn;
+    });
+  }
+
+  logout(): void {
+      this.authService.logout()
+        .subscribe( {
+          next: () => {
+            this.doLogout();
+          },
+          error: () => {
+            this.doLogout();
+          }
+        })
+  }
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы вышли из системы');
+    this.router.navigate(['/']);
+  }
+
+}
