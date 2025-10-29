49efc7f46a378d8e4d4ac0420d1196946de247df
«AlfidaE» <ermukhametovaalfida@mail.ru>
2025-10-30 02:50:28 +0500

    Five

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -15,8 +15,10 @@ export class OrderService {
 
   createOrder(params: OrderType): Observable<OrderType | DefaultResponseType> {
     return this.http.post<OrderType | DefaultResponseType>(environment.api + 'orders', params, {withCredentials: true})
+  }
 
-
+  getOrders(): Observable<OrderType[] | DefaultResponseType> {
+    return this.http.get<OrderType[] | DefaultResponseType>(environment.api + 'orders')
   }
 
 
